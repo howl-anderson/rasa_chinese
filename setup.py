dcd65f5b7be9b50c7ae3b518cefa10621647e00f
Xiaoquan Kong <u1mail2me@gmail.com>
2020-12-22 13:51:43 +0800

    fix version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='rasa_chinese',
-    version='0.0.1',
+    version='2.1.0',
     packages=find_packages(),
     url='https://github.com/howl-anderson/rasa_chinese',
     license='Apache 2.0',
     author='Xiaoquan Kong',
     author_email='u1mail2me@gmail.com',
     description='A Chinese language extension package for Rasa',
-    install_requires=["rasa~=2.2"]
+    install_requires=["rasa~=2.1"]
 )
